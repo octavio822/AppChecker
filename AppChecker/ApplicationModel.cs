7bf5ebe098d1a59cf610b4fd0fd6c819562fa9d8
unknown <octavio@JIGS-40F6853-FBC.sony-ste.sel.sony.com>
2026-02-18 07:43:28 -0800

    First Commit

---
+++ AppChecker/ApplicationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppChecker
+{
+    public class Applications : List<ApplicationModel>
+    {
+
+    }
+    public class ApplicationModel
+    {
+        public string name { get; set; }
+        public string process { get; set; }
+        public State State { get; set; }
+    }
+    public enum State
+    {
+        Running = 0,
+        Stopped = 1
+    }
+}
---
+++ AppChecker/Mail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace AppChecker
+{
+    public class Mail
+    {
+        private readonly string[] to = ConfigurationManager.AppSettings["to"].Split(',');
+
+        internal void Send(List<ApplicationModel> application)
+        {
+            string body = "<h1>Application Checker Results ([Host])</h1>";
+            body += "<table style=\"color: #333333; backgroud-color: #000000; width: 80%;\" border=\"1\" cellspacing=\"1\" cellpadding=\"4\">";
+            body += "<thead style=\"color: white; background-color: #5d7b9d; font-weight: bold;\"><tr><td>Application Name</td><td><p>Executable</p></td><td>State</td></tr></thead>";
+            body += "<tbody>";
+            foreach (ApplicationModel item in application)
+            {
+                body += String.Format("<tr {3}><td>{0}</td><td>{1}</td><td>{2}</td></tr>", item.name, item.process, item.State,item.State==State.Stopped? "style=\"background-color: #b50707; color: #ffffff;\"":"");
+            }
+            body += "</tbody></table>";
+            body += "<p><strong>For remove application Check File:</strong></p>";
+            body += "<pre>Config.json</pre>";
+
+            MailMessage message = new MailMessage();
+             message.Body = body;
+            message.Subject = "Application Status";
+            foreach (string item in to)
+            {
+                message.To.Add(item);
+            }
+            message.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient();
+            client.Send(message);
+        }
+
+    }
+}
